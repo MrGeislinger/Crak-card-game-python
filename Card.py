532d4bfb2536bf88ae353647bbac673d9445a000
VictorLoren <VictorLoren@users.noreply.github.com>
2015-01-12 17:08:59 -0800

    Create Card.py   - Gives definition of the Card class that will be used in Hands and Decks.   - Defined by a value and a suit. Note that the value 10 is defined as 'T' for consistency.   - Card is represented as a tuple of two single character strings. Example: 2 of hearts is represented as ('2','H').

---
+++ Card.py
@@ -0,0 +1,20 @@
+#Card
+class Card:
+	#Define the possible suits and values
+	suits  = ['H','D','S','C']
+	values = [str(x) for x in range(2,10)] #2-9 cards
+	values.extend(['T','J','Q','K','A']) #Face cards (including the 10s)
+
+	#Initiate card
+	def __init__(self,value,suit):
+		#Check to see if valid inputs, else return False
+		if suit in suits:
+			self.suit  = suit
+		else:
+			return False
+		if value in values:
+			self.value = value
+		else:
+			return False
+		#Return a card representation
+		return (self.value, self.suit)
