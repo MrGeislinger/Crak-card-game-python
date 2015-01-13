b3b164d0eeb2e3428f2e041b166d1dd07d2ecf34
VictorLoren <VictorLoren@users.noreply.github.com>
2015-01-12 18:00:13 -0800

    Update Card.py   - Fixes initation so that if a value or suit is not valid, give that value or suit a None value.

--- Card.py
+++ Card.py
@@ -7,14 +7,8 @@ class Card:
 
 	#Initiate card
 	def __init__(self,value,suit):
-		#Check to see if valid inputs, else return False
-		if suit in self.suits:
-			self.suit  = suit
-		else:
-			return False
-		if value in self.values:
-			self.value = value
-		else:
-			return False
-		#Return a card representation
-		return (self.value, self.suit)
+		#Check to see if valid inputs, else make it None value
+		self.suit = suit if suit in self.suits else None 
+		self.value = value if value in self.values else None
+		#Create card representation
+		self.card = (self.value, self.suit)
