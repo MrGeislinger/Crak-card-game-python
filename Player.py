0be345f0a1a0035e75c75f71b92c9caef55af851
VictorLoren <VictorLoren@users.noreply.github.com>
2015-01-13 19:04:27 -0800

    Update Player.py   - Makes initiation of a Player object instance also initiates a Hand object for the various hands.

--- Player.py
+++ Player.py
@@ -1,9 +1,11 @@
+import Hand from Hand
+
 #Player for the game of Crak
 class Player:
 	#Initiate the player with a name
 	def __init__(self,name):
 		self.name = name
-		#Define the different hands
-		self.handMine = [] #only Player sees
-		self.handShow = [] #all players see
-		self.handHide = [] #no players see
+		#Define the different hands (empty)
+		self.handMine = Hand() #only Player sees
+		self.handShow = Hand() #all players see
+		self.handHide = Hand() #no players see
